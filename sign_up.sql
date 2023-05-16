6778e26ed4d55a418de21339dd5d6a512edbebb3
EdAl004 <129920272+EdAl004@users.noreply.github.com>
2023-05-15 18:26:46 -0700

    Create sign_up.sql
    This is the sql code to create the table sign_up. The database MUST be named boba in order for the rest of the website to work.

---
+++ sign_up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE sign_up(
+   user_id    INT  NOT NULL AUTO_INCREMENT PRIMARY KEY 
+  ,first_name VARCHAR(50) NOT NULL
+  ,last_name  VARCHAR(50) NOT NULL
+  ,email      VARCHAR(75) NOT NULL
+);
+INSERT INTO sign_up(user_id,first_name,last_name,email) VALUES (1,'Edgar','Alatorre','123fake@gmail.com');
